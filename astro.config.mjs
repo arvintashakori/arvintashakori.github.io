7903db3ced8c889d8451598860e16c3ffa0b0d78
arvintashakori <arvin.ehsan.tashakori@gmail.com>
2023-07-23 16:53:07 -0400

    RVn: init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,6 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind(), image(
-    {
-      serviceEntryPoint: '@astrojs/image/sharp',
-      cacheDir: "./.cache/image",
-      logLevel: 'debug',
-    }
-  )]
-});
+  site: 'https://arvintashakori.github.io',
+  base: '/my-repo',
+})
